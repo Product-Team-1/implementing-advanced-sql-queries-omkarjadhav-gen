2fd35775df4d8aadb8e76e9445e7e26d410343e4
omkarjadhav-gen <167734630+omkarjadhav-gen@users.noreply.github.com>
2025-08-25 15:04:36 +0530

    Modified task

--- learner/query6.js
+++ learner/query6.js
@@ -4,7 +4,7 @@ async function executeQuery() {
   await setupDatabase();
 
   const result = await knex.raw(`
-    SELECT Gender, SUM(PostLikes) as TotalPostLikes
+    SELECT Gender, AVG(PostLikes) as TotalPostLikes
     FROM user_data
     INNER JOIN trending_posts ON User_ID = UserID
     GROUP BY Gender;
